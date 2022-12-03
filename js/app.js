a0b6e018ea761acbdfcc1cd871ce8c72f26a68f3
Adrienne Frey <AdrienneN.Frey@gmail.com>
2022-12-02 16:53:44 -0800

    about me site

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ console.log("Hey Hey!");
 
 let score = 0;
 
-//TODO: GET USERS NAME - PROMPT - alert a greeting back to the user
 
 function userName() {
   let userName = prompt('What is your name');
@@ -85,9 +84,6 @@ relationshipStatus();
 
 alert('Thanks for getting through my questions!');
 
-//TODO: prompt them with 5 yes or no (y/n) question, alert if they got the answer right or wrong
-
-//TODO:GIVE PERSONALIZED MESSAGE AT THE END
 
 function numberGame() {
   let myNum = 17; 
@@ -95,7 +91,7 @@ function numberGame() {
   for (let i = 0; i < 4; i++) { 
     
     let question6Response = prompt ('What number am I thinking of?');
-    // Runs 4 times, with values of step 0 through 4.
+    
     if (question6Response === myNum) {
       alert('That is correct');
       score++;
